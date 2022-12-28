0b791f95f74bcc498f14f07be951f11da80ff625
tmohney7 <mohney.t@hotmail.com>
2022-12-28 17:34:16 +0000

    Saved progress on Instantiation 2022-12-28 17:34:16 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        String thisobject = new String();
+        return thisobject;
     }
 }
