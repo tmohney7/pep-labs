143749833e33041f165958cfcff1daa132e6961e
tmohney7 <mohney.t@hotmail.com>
2023-01-02 22:03:22 +0000

    Saved progress on UncheckedException 2023-01-02 22:03:22 +0000

--- UncheckedException/src/main/java/CauseUncheckedException.java
+++ UncheckedException/src/main/java/CauseUncheckedException.java
@@ -14,6 +14,6 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        throw new RuntimeException();
     }
 }
