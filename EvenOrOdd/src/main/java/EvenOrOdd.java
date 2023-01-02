257a2dd1c0dadf01dd240d1b270e2799dc58a24f
tmohney7 <mohney.t@hotmail.com>
2023-01-02 19:03:46 +0000

    Saved progress on EvenOrOdd 2023-01-02 19:03:46 +0000

--- EvenOrOdd/src/main/java/EvenOrOdd.java
+++ EvenOrOdd/src/main/java/EvenOrOdd.java
@@ -1,3 +1,5 @@
+import javax.lang.model.util.ElementScanner6;
+
 public class EvenOrOdd {
     /**
      * Your challenge is to implement a method to determine if an int is even or odd. The method should return
@@ -14,6 +16,9 @@ public class EvenOrOdd {
      * @return a String "Even" if n is even, and "Odd" of in is odd. The test cases are case-sensitive.
      */
     public String decide(int n){
-        return "";
+        if (n % 2 == 0){
+            return "Even";}
+        else {
+            return "Odd";
     }
-}
+}}
