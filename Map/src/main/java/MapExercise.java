ebf9526f0b860b512bd1ec4daf7e53d7eef1a0f3
tmohney7 <mohney.t@hotmail.com>
2023-01-04 19:44:38 +0000

    Saved progress on Map 2023-01-04 19:44:38 +0000

--- Map/src/main/java/MapExercise.java
+++ Map/src/main/java/MapExercise.java
@@ -1,5 +1,6 @@
 
 import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Maps are key/value pairs. They are used in situations where we have some value that we'd like
@@ -26,7 +27,8 @@ public class MapExercise {
      * so here Integer is a wrapper for int.
      */
     public Map<Integer, String> createMap(){
-        return null;
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        return map;
     }
 
     /**
@@ -35,8 +37,9 @@ public class MapExercise {
      * @return the size of map.
      */
     public int getSize(Map<Integer,String> map){
+        int size = map.size();
 //        return total number of key/value pairs in the map
-        return 0;
+        return size;
     }
 
     /**
@@ -47,6 +50,7 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addKeyValuePair(Map<Integer,String> map, int key, String value){
+        map.put(key, value);
     }
 
     /**
@@ -56,7 +60,8 @@ public class MapExercise {
      * @return the value associated with key in map.
      */
     public String getValueFromKey(Map<Integer, String> map, int key){
-        return "";
+        String value = map.get(key);
+        return value;
     }
 
     /**
@@ -66,6 +71,7 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeKeyValuePair(Map<Integer, String> map, int key){
+        map.remove(key);
     }
 
     /**
@@ -76,6 +82,6 @@ public class MapExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void overwriteValue(Map<Integer, String> map, int key, String value){
-
+        map.put(key, value);
     }
 }
