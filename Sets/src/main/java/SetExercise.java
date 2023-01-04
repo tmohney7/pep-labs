564dc17e8ef2bdebd0d218823f0674d9f0167456
tmohney7 <mohney.t@hotmail.com>
2023-01-04 19:07:06 +0000

    Saved progress on Sets 2023-01-04 19:07:06 +0000

--- Sets/src/main/java/SetExercise.java
+++ Sets/src/main/java/SetExercise.java
@@ -1,5 +1,6 @@
 
 import java.util.Set;
+import java.util.HashSet;
 
 /**
  * Sets are unordered collections of elements. Sets do not allow duplicates.
@@ -20,7 +21,9 @@ public class SetExercise {
      * @return a new Set<Integer> object.
      */
     public Set<Integer> createSet(){
-        return null;
+        HashSet<Integer> set = new HashSet<Integer>();
+        return set;
+
     }
 
     /**
@@ -29,7 +32,8 @@ public class SetExercise {
      * @return the size of set. (number of values)
      */
     public int getSize(Set<Integer> set){
-        return 0;
+        int size = set.size();
+        return size;
     }
 
     /**
@@ -39,7 +43,7 @@ public class SetExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addUniqueItem(Set<Integer> set, int item){
-
+        set.add(item);
     }
 
     /**
@@ -49,6 +53,6 @@ public class SetExercise {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeItem(Set<Integer> set, int item){
-
+        set.remove(item);
     }
 }
